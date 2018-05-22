772c0b3d510a7fa70b99a0c9cbcb69816a227400
lin.cai <lin.cai@DESKTOP-QR2QH22>
2018-05-22 13:49:25 +0800

    整合redis

--- src/main/java/com/gaoenergy/controller/UserController.java
+++ src/main/java/com/gaoenergy/controller/UserController.java
@@ -9,13 +9,19 @@
  */
 package com.gaoenergy.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gaoenergy.pojo.User;
+import com.gaoenergy.utils.JSONResult;
+import com.gaoenergy.utils.JsonUtils;
+import com.gaoenergy.utils.RedisOperator;
 
 /**
  * <p>
@@ -32,6 +38,9 @@ import com.gaoenergy.pojo.User;
 @RequestMapping(value = "/user")
 public class UserController {
 
+	@Autowired
+	private RedisOperator redis;
+	
 	@RequestMapping(value = "/getUser")
 	@ResponseBody
 	public Object getUser() {
@@ -43,5 +52,41 @@ public class UserController {
 		u.setDesc("hello imooc2~~");
 
 		return u;
+	}
+	
+	@RequestMapping(value = "/getRedisJson")
+	@ResponseBody
+	public JSONResult testJedis() {
+		User user = new User();
+		user.setAge(18);
+		user.setName("慕课网");
+		user.setPassword("123456");
+		user.setBirthday(new Date());
+		
+		
+		User u1 = new User();
+		u1.setAge(19);
+		u1.setName("imooc");
+		u1.setPassword("123456");
+		u1.setBirthday(new Date());
+		
+		User u2 = new User();
+		u2.setAge(17);
+		u2.setName("hello imooc");
+		u2.setPassword("123456");
+		u2.setBirthday(new Date());
+		
+		List<User> userList = new ArrayList<>();
+		userList.add(user);
+		userList.add(u1);
+		userList.add(u2);
+		
+		
+		redis.set("json:info:userlist", JsonUtils.objectToJson(userList), 2000);
+		
+		String userListJson = redis.get("json:info:userlist");
+		List<User> userListBorn = JsonUtils.jsonToList(userListJson, User.class);
+		
+		return JSONResult.ok(userListBorn);
 	}
 }
